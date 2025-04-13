a466582963889f6f44f4d0c97e70694c7d917933
BasitIqbalatgit <rai.xardar.050@gmail.com>
2025-04-14 01:39:26 +0500

    commiting some more changes

--- app/api/admins/[id]/route.ts
+++ app/api/admins/[id]/route.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any,  @typescript-eslint/no-unused-vars*/
+
 // /app/api/admins/[id]/route.ts
 
 import { NextRequest, NextResponse } from 'next/server';
